77910ae417ba06ac8360c9b503a8e8a1cc4ed028
Kiran Anand <liteninkiran@hotmail.com>
2026-09-02 18:16:35 +0100

    016 - Creating writeable signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,54 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { BehaviorSubject, combineLatest, debounceTime, firstValueFrom, map } from 'rxjs';
-
-type Options = Record<string, string>;
-
-const initialOptions: Options = {
-  r: 'Red',
-  g: 'Green',
-  b: 'Blue',
-};
-
-const secondaryOptions: Options = {
-  m: 'Magenta',
-  y: 'Yellow',
-  c: 'Cyan',
-};
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { mySignal } from './my-signal';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  readonly a$ = new BehaviorSubject<number>(1);
-  readonly b$ = new BehaviorSubject<number>(2);
-  readonly sum$ = combineLatest([this.a$, this.b$]).pipe(map(([a, b]) => a + b));
+  // readonly firstSignal = signal(42);
+  // readonly secondSignal = signal('Signals');
 
-  async incA() {
-    // Only increment A if A + B is less than 10
-    const sum = await firstValueFrom(this.sum$);
-    if (sum < 10) {
-      this.a$.next(this.a$.value + 1);
-    }
-  }
-
-  readonly options$ = new BehaviorSubject<Options>(initialOptions);
-  readonly selectedKey$ = new BehaviorSubject<string>('b');
-  readonly selectedValue$ = combineLatest([this.options$, this.selectedKey$]).pipe(
-    debounceTime(0),
-    map(([options, key]) => options[key]),
-  );
+  readonly firstSignal = mySignal(42);
+  readonly secondSignal = mySignal('Signals');
 
   constructor() {
-    this.selectedValue$.subscribe(console.log);
-  }
-
-  switchOptions() {
-    this.options$.next(secondaryOptions);
-    this.selectedKey$.next('c');
+    console.log('The first signal value is:', this.firstSignal());
+    console.log('The second signal value is:', this.secondSignal());
   }
 }
