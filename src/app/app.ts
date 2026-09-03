94bfbd20fba826af3fc268fec312dee882624588
Kiran Anand <liteninkiran@hotmail.com>
2026-09-03 20:02:41 +0100

    031 - Interoperability with rxjs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,29 +1,35 @@
-import { CommonModule } from '@angular/common';
-import { Component, computed, effect, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ApiService } from './api.service';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  readonly x = signal(10);
-  readonly isLarge = signal(false);
-
-  readonly xLarge = computed(() => this.x() > 12);
-
-  incrementX() {
-    this.x.update((v) => v + 1);
-  }
+  readonly number = signal(10);
+  readonly number$ = toObservable(this.number);
+  readonly results$ = this.number$.pipe(switchMap((n) => this.api.getPrimeFactors(n)));
+  readonly api = inject(ApiService);
+  readonly primeFactors = toSignal(this.results$, {
+    initialValue: [],
+  });
 
   constructor() {
-    effect(() => {
-      if (this.x() > 12) {
-        console.log('x is greater than 12');
-        this.isLarge.set(true);
-      }
+    this.number$.subscribe((n) => {
+      console.log('Number changed to', n);
     });
+  }
+
+  increase() {
+    this.number.update((n) => n + 1);
+  }
+
+  decrease() {
+    this.number.update((n) => Math.max(n - 1, 3));
   }
 }
